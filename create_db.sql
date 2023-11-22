eb7b2ae8ab1be5c142b550452714bf282607d637
Flamberger <hwangernhong@gmail.com>
2023-11-22 13:25:18 +0000

    Initial commit  Changes to be committed: 	new file:   create_db.sql 	new file:   index.js 	new file:   insert_test_data.sql 	new file:   package-lock.json 	new file:   package.json 	new file:   public/main.css 	new file:   routes/main.js 	new file:   views/about.ejs 	new file:   views/index.ejs 	new file:   views/register.ejs 	new file:   views/search.ejs

---
+++ create_db.sql
@@ -0,0 +1,11 @@
+# Create database script for Berties books
+
+# Create the database
+CREATE DATABASE myBookstore;
+USE myBookstore;
+
+# Create the tables
+CREATE TABLE books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
+GRANT ALL PRIVILEGES ON myBookstore.* TO 'appuser'@'localhost';
+

89652fd22faedf6beb7bdd5d23a73f6d2cb7380a
Flamberger <hwangernhong@gmail.com>
2023-12-16 18:41:31 +0000

    modified:   create_db.sql 	deleted:    insert_test_data.sql 	modified:   routes/main.js 	new file:   views/addtopic.ejs 	modified:   views/index.ejs 	modified:   views/index2.ejs 	modified:   views/posts.ejs 	modified:   views/profile.ejs 	modified:   views/register.ejs 	modified:   views/topicProfile.ejs

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ module.exports = function(app, forumData) {
     });
 
     app.get('/about',function(req,res){
-        res.render('about.ejs', forumData);
+        res.render('about.ejs', forumData);``
     });
 
     app.get('/search',function(req,res){
@@ -55,13 +55,42 @@ module.exports = function(app, forumData) {
             if(req.body.username == result[0].username && req.body.password == result[0].password){
                 session=req.session;
                 session.userid=req.body.username;
-                res.send(`Hey there, welcome ${req.body.username} <a href=\'/logout'>click to logout</a>`);
+                res.send(`Hey there, welcome ${req.body.username}! <a href="./">Click Here to go back to index</a>`);
             }
             else{
-                res.send('Invalid username or password');
+                res.send('Invalid username or password, <a href="/login">Click Here to try again</a></p>');
             }
         });
-    })
+    });
+    app.get('/jointopic/:topic', function(req,res) {
+        if (req.session && req.session.userid) {
+            let sqlquery = `
+            SELECT idusers, username
+            FROM users
+            WHERE username = ?
+            `
+            let sqlquery2 = `
+            SELECT idtopic, title
+            FROM topics
+            WHERE title = ?
+            `
+            let sqlinsert = `
+            INSERT INTO users_topics (idusers, idtopics)
+            VALUES (?, ?)
+            `
+            db.query(sqlquery, req.session.userid, (err, result) =>{
+                db.query(sqlquery2, req.params.topic, (err, result2) => {
+                    let newrecord = [result[0].idusers, result2[0].idtopic]
+                    console.log (newrecord);
+                    db.query(sqlinsert, newrecord, (err, result3) => {
+                        res.send('You are now registered with ' + req.params.topic + '. <a href="/">Click Here to go back to index.</a>')
+                    })
+                });
+            });
+        } else {
+            res.redirect('/login')
+        }
+    });
     app.get('/logout',(req,res) => {
         req.session.destroy();
         res.redirect('/');
@@ -71,8 +100,27 @@ module.exports = function(app, forumData) {
     });  
 
     app.post('/registered', function (req,res) {
-        // saving data in database
-        res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+        let sqlquery = `
+        SELECT username
+        FROM users
+        WHERE username = ?
+        `
+        let sqlquery2 = `
+        INSERT INTO users (username, password)
+        VALUES (?, ?)
+        `
+        newRecord = [req.body.username, req.body.password]
+        db.query(sqlquery, req.body.username, (err, result) => {
+            if (result.length < 1) {
+                db.query(sqlquery2, newRecord, (err, result) => {
+                    res.send('your user details have been added to database, username: '
+                    + req.body.username + '</br><p><a href="/">Click Here to go back to index</a></p>');
+                });                                       
+            } else {
+                res.send('<p>username '
+                    + req.body.username + ' is not unique.</p>' + '</br><p><a href="/register">Click Here to try again</a></p>');
+            }
+        });
     }); 
 
     app.get('/posts', function(req, res) {
@@ -87,7 +135,6 @@ module.exports = function(app, forumData) {
             if (err) {
                 res.redirect('./'); 
             }
-            // create object with both forumData and Books table as its elements
             let newData = Object.assign({}, forumData, {posts:result});
             console.log(newData);
             res.render("posts.ejs", newData)
@@ -115,16 +162,28 @@ module.exports = function(app, forumData) {
         JOIN users ON users_topics.idusers = users.idusers
         JOIN topics ON users_topics.idtopics = topics.idtopic
         WHERE username = ?`
+        let sqlquery2 = `
+        SELECT users.username, topics.title, header, body
+        FROM posts
+        JOIN topics ON posts.idtopic = topics.idtopic
+        JOIN users ON posts.idcreator = users.idusers
+        WHERE username = ?
+        `
         db.query(sqlquery, req.params.username, (err, result) => {
             if (err) {
                 res.redirect('./'); 
             }
+            db.query(sqlquery2, req.params.username, (err, result2) => {
+                if (err) {
+                    res.redirect('./'); 
+                }
             // create object with both forumData and Books table as its elements
-            let newData = Object.assign({}, forumData, {user: req.params.username}, {topics:result});
+            let newData = Object.assign({}, forumData, {user: req.params.username}, {topics:result}, {posts:result2});
             console.log(newData);
             res.render("profile.ejs", newData)
-        })
-    })
+            });
+        });
+    });
     app.get('/topics', function(req, res) {
         let sqlquery = `
         SELECT title, description
@@ -228,6 +287,29 @@ module.exports = function(app, forumData) {
                   }
                 });
             });
+        });
+    });
+    app.get('/addtopic', (req, res) => {
+        if (req.session && req.session.userid) {
+                res.render("addtopic.ejs", forumData)
+        } else {
+            res.redirect('/login');
+        }
+    })
+
+    app.post('/topicadded', function (req,res) {
+        let sqlquery = "INSERT INTO topics (title, description) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.title, req.body.description];
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+            return console.error(err.message);
+            }
+            else {
+            res.send('your topic has been added to database, titled: '
+                        + req.body.title + ', description: ' + req.body.description 
+                        + '</br><p><a href="./">Click Here to go back to index</a></p>');
+            }
         });
     });
 }

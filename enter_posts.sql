6e612145f7f853c36fa3cc4c4e94d1cb97017b78
Flamberger <hwangernhong@gmail.com>
2023-12-16 19:21:47 +0000

    modified:   enter_posts.sql

--- enter_posts.sql
+++ enter_posts.sql
@@ -1,4 +1,6 @@
-INSERT INTO `topics` VALUES (1,'Market gods','Worship the lords of finance. The great invisibles hands that move market and soul. Revel in the scales of which dominate and overshadow even those that profit from them.'),
-(2,'Primal horror','Engage the techniques of old. Gain power. Embrace divinity. Invest now.'),
-(3,'Basketball','Talk about basketball'),
-(4,'Computer Games','I heart Dopamine. I heart feelings.');
+INSERT INTO `posts` VALUES (1,1,1,'Engorged Flesh','Help my security postulates has grown cancerous. Pus yield seems poor, I want to pop it now and bail. Should have diversified my bile. Take it from me this was a bad investment.'),
+(2,3,3,'I love basketball','BASKETBALL!!!'),
+(3,2,2,'Sacrifices are good this year','AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'),
+(9,3,1,'koby','the bronze jade'),
+(10,1,4,'I think the world is ending','I don\'t think the markets are trying to pander to me anymore. That was all the craving I ever had in my life. Without these megacorps feeling like they need to appease me for their livelihoods, I am just another worthless maggot rolling around on this fetid Earth. Why bother at this point?'),
+(11,4,3,'Jimbo\'s Futility','I got this game last year and it kind of sucks ngl. Been playing it on and off but the core gameplay loop just doesn\'t hit after more than an hour. Ig the part where a ghost reaches through the screen to squeeze your heart for 3 seconds just gets old fast. Maybe if they added a permadeath system, it wouldn\'t feel so mundane.');

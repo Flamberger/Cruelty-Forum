9124b77da8103a91bf7bee578e492055a29b100a
Flamberger <hwangernhong@gmail.com>
2023-12-16 19:12:46 +0000

    new file:   enter_posts.sql

---
+++ enter_posts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `topics` VALUES (1,'Market gods','Worship the lords of finance. The great invisibles hands that move market and soul. Revel in the scales of which dominate and overshadow even those that profit from them.'),
+(2,'Primal horror','Engage the techniques of old. Gain power. Embrace divinity. Invest now.'),
+(3,'Basketball','Talk about basketball'),
+(4,'Computer Games','I heart Dopamine. I heart feelings.');
